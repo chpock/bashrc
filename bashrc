47420dada0beac625b66973a47839d117b8e3e78
Kostiantyn Kushnir <chpock@gmail.com>
2026-07-15 16:15:10 +0300

    feat(k9s): add ephemeral debug plugin

--- bashrc
+++ bashrc
@@ -515,7 +515,7 @@ EOF
 
 # avoid issue with some overflow when the file is more than 65536 bytes
 cat <<'EOF' > "$IAM_HOME/bashrc"
-LOCAL_TOOLS_FILE_HASH=6AF722C0
+LOCAL_TOOLS_FILE_HASH=A31F22C3
 BASHRC_FILE_HASH=5537DB6B
 declare -A -r __CPRINTF_COLORS=(
 [fw]=$'\e[37m' [fW]=$'\e[97m'
--- functions/k9s.sh
+++ functions/k9s.sh
@@ -109,5 +109,87 @@ plugins:
     args:
     - -e
     - "kubectl logs $POD -c $NAME -n $NAMESPACE --context $CONTEXT -f"
+  debug-ephemeral:
+    shortCut: Shift-D
+    confirm: false
+    description: "Debug (Ephemeral)"
+    scopes:
+      - pods
+      - containers
+    command: sh
+    background: false
+    args:
+      - -c
+      - sh "$K9S_CONFIG_DIR"/k9s-debug.sh "$CONTEXT" "$NAMESPACE" "$POD" "$NAME" "$CONTAINER"
+
+---- k9s-debug.sh
+#!/bin/sh
+# Map cluster contexts to their respective private debug images
+
+KUBE_CONTEXT="$1"
+POD_NAMESPACE="$2"
+POD_NAME="$3"
+
+if [ -z "$POD_NAME" ]; then
+    POD_NAME="$4"
+    TARGET_CONTAINER=''
+else
+    TARGET_CONTAINER="$4"
+fi
+
+SPARK_FAB_DEBUG_IMAGE="spark-fab/tools-debug-image:main-260715.0"
+
+# Exit if pod name is missing
+if [ -z "$POD_NAME" ]; then
+    echo "Error: Pod name is required."
+    sleep 2
+    exit 1
+fi
+
+# Define private registry mappings based on kubeconfig context
+case "$KUBE_CONTEXT" in
+    spark-fab-*-nbprod)
+        DEBUG_IMAGE="ecr.techdev.ormcodigital.com/$SPARK_FAB_DEBUG_IMAGE"
+        ;;
+    sparkfab-*-eks)
+        DEBUG_IMAGE="448049824165.dkr.ecr.us-east-1.amazonaws.com/$SPARK_FAB_DEBUG_IMAGE"
+        ;;
+    *)
+        DEBUG_IMAGE="docker.io/nicolaka/netshoot:latest" # Default fallback
+        ;;
+esac
+
+echo "Starting ephemeral debug session in context: $KUBE_CONTEXT"
+echo "Image: $DEBUG_IMAGE"
+echo
+echo "Pod      : $POD_NAME"
+echo "Namespace: $POD_NAMESPACE"
+echo "Container: $TARGET_CONTAINER"
+echo
+
+set -- kubectl --context "$KUBE_CONTEXT" \
+    --namespace "$POD_NAMESPACE" \
+    debug -it "$POD_NAME" \
+    --image="$DEBUG_IMAGE" \
+    --profile=sysadmin \
+    --env="TERM=${TERM:-xterm}" \
+    --share-processes
+
+# Execute kubectl debug with ephemeral container
+# If a specific container is selected, share its process namespace
+if [ -n "$TARGET_CONTAINER" ]; then
+    set -- "$@" --target="$TARGET_CONTAINER"
+fi
+
+SHELL_DISCOVERY_CMD="[ -x /bin/bash ] && exec /bin/bash || [ -x /usr/bin/bash ] && exec /usr/bin/bash || exec /bin/sh"
+
+set -- "$@" -- sh -c "$SHELL_DISCOVERY_CMD"
+
+if ! "$@"; then
+    echo
+    echo "ERROR: something was wrong!"
+    sleep 5
+    exit 1
+fi
 ----
 }
